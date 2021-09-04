39d12cca8f3d6fe9a9a6b3b85e35f126d54b9e8b
Nathan Sadala <bernynath@gmail.com>
2021-09-05 00:28:15 +0200

    adding persisted store

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex)
 
@@ -25,6 +26,7 @@ export default new Vuex.Store({
   mutations: mutations,
   actions: actions,
   getters: getters,
-  modules: {
-  }
+  plugins: [createPersistedState({
+    storage: window.sessionStorage()
+  })]
 })
